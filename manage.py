6948dd434a9de545ea3f6d79c98778507aa7b883
jack ogina <jackogina60@gmail.com>
2018-05-20 06:20:34 -0400

    added a server command for debugging during development

--- manage.py
+++ manage.py
@@ -1,7 +1,8 @@
 
 from flask_dbshell import DbShell
 from flask_migrate import Migrate, MigrateCommand
-from flask_script import Manager, Shell
+from flask_script import Manager, Shell,Server
+
 
 from pitches.app import db,create_app
 from pitches.models import User, Role, Permission, Topic, TopicGroup, Comment, PollVote, PollAnswer, Message
@@ -17,7 +18,7 @@ def make_shell_context():
     return dict(app=app, db=db, User=User, Role=Role, Permission=Permission, Topic=Topic, TopicGroup=TopicGroup,
                 Comment=Comment, PollVote=PollVote, PollAnswer=PollAnswer, Message=Message)
 
-
+manager.add_command('server',Server)
 manager.add_command('shell', Shell(make_context=make_shell_context))
 manager.add_command('db', MigrateCommand)
 
@@ -36,6 +37,25 @@ def test():
     import unittest
     tests = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner(verbosity=2).run(tests)
+
+@manager.command
+def insert_initial_data():
+    """Adds initial data to database."""
+    Role.insert_roles()
+    TopicGroup.insert_root_topic_group()
+
+
+@manager.command
+def insert_fake_data():
+    """Adds fake data to database."""
+    from utils import data_generator
+    data_generator.generate_fake_users()
+    data_generator.generate_fake_topic_groups()
+    data_generator.generate_fake_topics()
+    data_generator.generate_fake_comments()
+    data_generator.generate_fake_messages()
+    data_generator.generate_fake_polls()
+    data_generator.generate_fake_votes()
 
 
 
